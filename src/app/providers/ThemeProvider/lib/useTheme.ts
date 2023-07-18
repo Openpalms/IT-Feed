a143fb5fed62bcfa61b3f43adbcb8b8f2d3817a9
ga104kin <ga104kin@M2.local>
2023-07-18 18:04:08 +0300

    fix themes for modal window

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -1,16 +1,17 @@
-import { useContext } from 'react';
-import { LOCAL_STORAGE_THEME_KEY, THEMES, ThemeContext } from './ThemeContext';
+import { useContext } from 'react'
+import { LOCAL_STORAGE_THEME_KEY, THEMES, ThemeContext } from './ThemeContext'
 
 interface UseThemeResult {
-  toggleTheme: () => void;
-  theme: THEMES;
+  toggleTheme: () => void
+  theme: THEMES
 }
 export function useTheme(): UseThemeResult {
-  const { theme, setTheme } = useContext(ThemeContext);
+  const { theme, setTheme } = useContext(ThemeContext)
   const toggleTheme = () => {
-    const newTheme = theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
-    setTheme(newTheme);
-    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
-  };
-  return { theme, toggleTheme };
+    const newTheme = theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT
+    setTheme(newTheme)
+    document.body.className = newTheme
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
+  }
+  return { theme, toggleTheme }
 }
